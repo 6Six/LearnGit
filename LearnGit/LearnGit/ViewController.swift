7e7d506e6215bc09787942e14b9a3001c83c3a20
Garryzhang <hgzzl1989@126.com>
2016-01-04 16:49:10 +0800

    Create project.

---
+++ LearnGit/LearnGit/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LearnGit
+//
+//  Created by Garry on 16/1/4.
+//  Copyright © 2016年 com.garry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("This is used for learning git");
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
